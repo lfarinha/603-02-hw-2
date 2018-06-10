d7d1c166359e3e48ba9de819cf03394b856ccf19
leonardo farinha <leonardofarinhap@gmail.com>
2018-06-10 18:10:02 -0500

    Done last exercise

--- H-2/interest.py
+++ H-2/interest.py
@@ -2,3 +2,18 @@
 
 #https://runestone.academy/runestone/static/thinkcspy/SimplePythonData/Exercises.html
 #excercise 7
+
+p = float(input("Enter the amount of the principal: "))
+t = int(input("Enter the number of years: "))
+r = float(input("Enter the nominal interest: "))
+n = 12
+
+a = (1 + (r / n))
+
+result = 1
+for x in range(t*n):
+    result *= a
+
+result = p * result
+
+print("Result: ", result)
